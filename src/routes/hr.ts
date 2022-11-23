fd0c68113e7f68315567ea2131e687bcb075a320
Naphattharawat S <naphattharawat@gmail.com>
2022-11-23 16:21:35 +0700

    update

--- src/routes/hr.ts
+++ src/routes/hr.ts
@@ -18,14 +18,18 @@ router.get('/', async (req: Request, res: Response) => {
   try {
     const decoded = req.decoded;
     console.log(decoded);
-    
+
     if (decoded.cid) {
       const c: any = await hrModel.checkToken(accessToken, refreshToken);
       if (c.ok) {
         accessToken = c.access_token;
         refreshToken = c.refresh_token;
         const rs: any = await hrModel.getData(decoded.cid, accessToken);
-        res.send(rs);
+        if (rs.data) {
+          res.send(rs);
+        } else {
+          res.send({ ok: false, error: 'ไม่พบข้อมูล' })
+        }
       } else {
         res.send({ ok: false, error: 'ไม่พบข้อมูล' })
       }
