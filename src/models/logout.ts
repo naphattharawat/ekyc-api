e4324f13fa69f8c27941db0126f82847a04b4f9a
Naphattharawat S <naphattharawat@gmail.com>
2022-12-14 21:34:51 +0700

    update

--- src/models/logout.ts
+++ src/models/logout.ts
@@ -3,11 +3,11 @@ const request = require('request');
 export class LogoutModel {
 
   setStatus(db, deviceId, status) {
-    return db('devlices')
+    return db('devices')
       .update('status', status)
       .where('device_id', deviceId);
   }
-  
+
   saveLog(db, deviceId, type) {
     return db('logs_login').insert({
       device_id: deviceId, type
