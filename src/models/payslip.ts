f5fe94b8d2c06c1fe80857388b746db8af6729b3
Naphattharawat S <naphattharawat@gmail.com>
2023-01-10 11:00:14 +0700

    1.3.9+32

--- src/models/payslip.ts
+++ src/models/payslip.ts
@@ -3,44 +3,52 @@ const request = require('request');
 export class PaySlipModel {
 
     getSlip(accessToken, yy, mm) {
-
-        const options = {
-            method: 'GET',
-            url: `https://payslip-ops.moph.go.th/api/v1/m/user/slip/${yy}/${mm}`,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': `Bearer ${accessToken}`,
-            }
-        };
-        return new Promise<void>((resolve, reject) => {
-            request(options, function (error, response, body) {
-                if (error) {
-                    reject(error)
-                } else {
-                    resolve(JSON.parse(body))
+        try {
+            const options = {
+                method: 'GET',
+                url: `https://payslip-ops.moph.go.th/api/v1/m/user/slip/${yy}/${mm}`,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': `Bearer ${accessToken}`,
                 }
+            };
+            return new Promise<void>((resolve, reject) => {
+                request(options, function (error, response, body) {
+                    if (error) {
+                        reject(error)
+                    } else {
+                        resolve(JSON.parse(body))
+                    }
+                });
             });
-        });
+        } catch (error) {
+            return [];
+        }
     }
 
     getSlips(accessToken) {
-        const options = {
-            method: 'GET',
-            url: 'https://payslip-ops.moph.go.th/api/v1/m/user/slips',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': `Bearer ${accessToken}`,
-            }
-        };
-        return new Promise<void>((resolve, reject) => {
-            request(options, function (error, response, body) {
-                if (error) {
-                    reject(error)
-                } else {
-                    resolve(JSON.parse(body))
+        try {
+            const options = {
+                method: 'GET',
+                url: 'https://payslip-ops.moph.go.th/api/v1/m/user/slips',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': `Bearer ${accessToken}`,
                 }
+            };
+            return new Promise<void>((resolve, reject) => {
+                request(options, function (error, response, body) {
+                    if (error) {
+                        reject(error)
+                    } else {
+                        resolve(JSON.parse(body))
+                    }
+                });
             });
-        });
+
+        } catch (error) {
+            return [];
+        }
     }
 
 
