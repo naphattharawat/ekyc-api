fa7eb0d4e8f25344bf17adb3ac1565e96f328d74
Naphattharawat S <naphattharawat@gmail.com>
2023-01-24 15:01:02 +0700

    fix error

--- typings.d.ts
+++ typings.d.ts
@@ -3,7 +3,7 @@ import * as Knex from 'knex';
 declare module 'express' {
   interface Request {
     db: any // Actually should be something like `multer.Body`
-    knex: Knex,
+    knex: any,
     decoded: any // Actually should be something like `multer.Files`.
     files: any
     mqttClient: any
