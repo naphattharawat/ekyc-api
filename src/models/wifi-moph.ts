b880aadd478f4deb3c40b425a04d69bce257a326
Naphattharawat S <naphattharawat@gmail.com>
2023-02-13 16:57:03 +0700

    update

---
+++ src/models/wifi-moph.ts
@@ -0,0 +1,21 @@
+import Knex = require("knex");
+
+
+
+export class WifiMophModel {
+
+  saveMacAddress(db: any, data) {
+    return db('device_wifi_moph').insert(data);
+  }
+
+
+  findMacAddress(db: any, deviceId: any) {
+    return db('device_wifi_moph').find({ device_id: deviceId }).where('is_deleted', 'N');
+  }
+
+  removeMacAddress(db: any, deviceId) {
+    return db('device_wifi_moph').update({ 'is_deleted': 'Y' });
+  }
+
+}
+
--- src/routes/wifi-moph.ts
+++ src/routes/wifi-moph.ts
@@ -6,32 +6,60 @@ import { Router, Request, Response } from 'express';
 import * as HttpStatus from 'http-status-codes';
 import * as crypto from 'crypto';
 
-import { LoginModel } from '../models/login';
 
 import { Jwt } from '../models/jwt';
+import { WifiMophModel } from '../models/wifi-moph';
 
-const deviceModel = new DeviceModel();
-const loginModel = new LoginModel();
+const wifiMophModel = new WifiMophModel
 const jwt = new Jwt();
 
 const router: Router = Router();
 
 router.post('/', async (req: Request, res: Response) => {
   try {
-    let refreshToken: string = req.body.refreshToken;
-    let rs: any = await loginModel.refreshToken(refreshToken);
-
-    if (rs.access_token) {
-      rs.ok = true;
-      res.send(rs);
+    const db = req.db;
+    const macAddress = req.body.macAddress;
+    const deviceId = req.body.deviceId;
+    if (deviceId && macAddress) {
+      const info = wifiMophModel.findMacAddress(db, deviceId);
+      if (info.length) {
+        await wifiMophModel.removeMacAddress(db, deviceId);
+      }
+      for (const i of process.env.FIREWALL_URL.split(',')) {
+        const obj = {
+          'device_id': deviceId,
+          'mac_address': macAddress,
+          'firewall_url': `${i}/api/v2/cmdb/firewall/address`,
+          cid: req.decoded.cid
+        }
+        await wifiMophModel.saveMacAddress(db, obj);
+      }
+      res.send({ ok: true });
     } else {
-      res.status(401)
-      res.send({ ok: false, error: rs.error, message: rs.message });
+      res.send({ ok: false, error: 'Parameter not found' });
     }
   } catch (error) {
     res.status(HttpStatus.BAD_GATEWAY);
     res.send({ ok: false, error: error.message });
   }
 });
+
+router.delete('/', async (req: Request, res: Response) => {
+  try {
+    const db = req.db;
+    const deviceId = req.body.deviceId;
+    if (deviceId) {
+      await wifiMophModel.removeMacAddress(db, deviceId);
+      res.send({ ok: true });
+    } else {
+      res.send({ ok: false, error: 'Parameter not found' });
+    }
+  } catch (error) {
+    res.status(HttpStatus.BAD_GATEWAY);
+    res.send({ ok: false, error: error.message });
+  }
+});
+
+
 
 export default router;
